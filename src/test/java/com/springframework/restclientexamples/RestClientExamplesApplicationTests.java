833a30c0219891042ec9eb499dbde017c0a4b2e1
djordje_radivojevic_eng <djordje.radivojevic.eng@gmail.com>
2020-11-03 17:34:56 +0100

    Initial commit

---
+++ src/test/java/com/springframework/restclientexamples/RestClientExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springframework.restclientexamples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestClientExamplesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
